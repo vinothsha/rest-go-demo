f6955d362d28a8ab1dcf755ff795b9dd868df023
vinothsha <kvinothsha@gmail.com>
2022-09-01 14:32:15 +0530

    commit

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 	"rest-go-demo/database"
 
 	// "github.com/gin-gonic/gin"
-	"github.com/gorilla/mux"
 	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/mysql" //Required for MySQL dialect
 )
 
@@ -19,18 +19,19 @@ func main() {
 	router := mux.NewRouter()
 	initaliseHandlers(router)
 
-	
 }
-
-func initaliseHandlers(router *mux.Router) {
+func CORS() (handlers.CORSOption, handlers.CORSOption, handlers.CORSOption) {
 	headers := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
 	methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"})
 	origins := handlers.AllowedOrigins([]string{"*"})
+	return headers, methods, origins
+}
+func initaliseHandlers(router *mux.Router) {
+	var headers, methods, origins handlers.CORSOption = CORS()
 	router.HandleFunc("/form", controllers.Create).Methods("POST")
-	router.HandleFunc("/get",controllers.Getall)
+	router.HandleFunc("/get", controllers.Getall)
 	http.ListenAndServe(":8090", handlers.CORS(headers, methods, origins)(router))
-	
-	
+
 }
 
 func initDB() {
